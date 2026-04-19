03084ec1411dd2a3a5e288f8c21e6ad26362e805
RichmondArconado <richmondarconado0@gmail.com>
2026-04-19 16:18:47 +0800

    Update

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-// WE DELETED "import './index.css'" HERE TO FIX THE WHITE SCREEN!
+import "./App.css"; // We ONLY want our Godly IDE styles
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
